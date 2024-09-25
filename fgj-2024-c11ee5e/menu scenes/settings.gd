7f73afb844d4041fd8ee877f0697e8b46ad38ba6
FERGUS CURRIE <2309394@uad.ac.uk>
2024-09-25 19:17:41 +0100

    added a settings menu that can be closed

--- fgj-2024-c11ee5e/menu scenes/settings.gd
+++ fgj-2024-c11ee5e/menu scenes/settings.gd
@@ -3,17 +3,21 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Panel/VBoxContainer/play.grab_focus()
-	$settings.visible = false
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
-func _on_quit_pressed():
-	get_tree().quit()
+func _on_sound_close_pressed():
+	$".".visible = false
 
 
-func _on_settings_pressed():
-	$settings.visible = true
+func _on_graphics_close_pressed():
+	$".".visible = false
+
+
+func _on_gameplay_close_pressed():
+	$".".visible = false
