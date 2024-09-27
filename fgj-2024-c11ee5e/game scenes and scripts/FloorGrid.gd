9c59b699c3e47b830f6c678c7fe99b122efce972
Fergus Currie <2309394@abertay.ac.uk>
2024-09-27 16:06:35 +0100

    added cheese

--- fgj-2024-c11ee5e/game scenes and scripts/FloorGrid.gd
+++ fgj-2024-c11ee5e/game scenes and scripts/FloorGrid.gd
@@ -5,6 +5,7 @@ extends Node2D
 func _ready():
 	$Control.visible = false
 	$"Control/settings menu".visible = false
+	$TileMap.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
