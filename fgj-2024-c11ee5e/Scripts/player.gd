21f912574d17baa8f0d173d6387612dbc125d365
Ross McCorkindale <ross.mccorkindale30@gmail.com>
2024-09-27 17:49:30 +0100

    Move counter added

--- fgj-2024-c11ee5e/Scripts/player.gd
+++ fgj-2024-c11ee5e/Scripts/player.gd
@@ -4,6 +4,7 @@ extends Node2D
 var timer = null
 var movement_delay = 0.35
 var can_move = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timer = Timer.new()
@@ -21,37 +22,41 @@ func _process(delta):
 
 func _input(event):
 	if Input.is_action_pressed("move_up") && can_move:
-		if position.y < 100:
+		if position.y < 100 or Global.move_counter >= Global.total_moves:
 			pass
 		else:
 			position.y -= 105
 			rotation = 0
 			can_move = false
 			timer.start()
+			Global.move_counter += 1
 	if Input.is_action_pressed("move_down") && can_move:
-		if position.y > 400:
+		if position.y > 400 or Global.move_counter >= Global.total_moves:
 			pass
 		else:
 			position.y += 105
 			rotation = 3.2
 			can_move = false
 			timer.start()
+			Global.move_counter += 1
 	if Input.is_action_pressed("move_left") && can_move:
-		if position.x < 100:
+		if position.x < 100 or Global.move_counter >= Global.total_moves:
 			pass
 		else:
 			position.x -= 105
 			rotation = 4.7
 			can_move = false
 			timer.start()
+			Global.move_counter += 1
 	if Input.is_action_pressed("move_right") && can_move:
-		if position.x > 400:
+		if position.x > 400 or Global.move_counter >= Global.total_moves:
 			pass
 		else:
 			position.x += 105
 			rotation = 1.5
 			can_move = false
 			timer.start()
+			Global.move_counter += 1
 	if event.is_action_released("move_up"):
 		can_move = true
 	if event.is_action_released("move_down"):
