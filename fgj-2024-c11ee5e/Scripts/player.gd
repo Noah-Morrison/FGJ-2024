85856535f200f8380a9f38686d870d58f6cf206e
Noah-Morrison <145436571+Noah-Morrison@users.noreply.github.com>
2024-09-28 18:09:54 +0100

    Cleaned Up Player Script

--- fgj-2024-c11ee5e/Scripts/player.gd
+++ fgj-2024-c11ee5e/Scripts/player.gd
@@ -5,6 +5,8 @@ var timer = null
 var movement_delay = 0.35
 var can_move = true
 
+var actions = ["move_up", "move_down", "move_left", "move_right"]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timer = Timer.new()
@@ -19,50 +21,58 @@ func on_timeout_complete():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func _input(event):
-	if Input.is_action_pressed("move_up") && can_move:
-		if position.y < 100 or Global.move_counter >= Global.total_moves:
+	
+func _commit_action(action):
+	var commit = false
+	if action == "move_up":
+		if position.y < 100:
 			pass
 		else:
 			position.y -= 105
 			rotation = 0
-			can_move = false
-			timer.start()
-			Global.move_counter += 1
-	if Input.is_action_pressed("move_down") && can_move:
-		if position.y > 400 or Global.move_counter >= Global.total_moves:
+			commit = true
+	elif action == "move_down":
+		if position.y > 400:
 			pass
 		else:
 			position.y += 105
 			rotation = 3.2
-			can_move = false
-			timer.start()
-			Global.move_counter += 1
-	if Input.is_action_pressed("move_left") && can_move:
-		if position.x < 100 or Global.move_counter >= Global.total_moves:
+			commit = true
+	elif action == "move_left":
+		if position.x < 100:
 			pass
 		else:
 			position.x -= 105
 			rotation = 4.7
-			can_move = false
-			timer.start()
-			Global.move_counter += 1
-	if Input.is_action_pressed("move_right") && can_move:
-		if position.x > 400 or Global.move_counter >= Global.total_moves:
+			commit = true
+	elif action == "move_right":
+		if position.x > 400:
 			pass
 		else:
 			position.x += 105
 			rotation = 1.5
-			can_move = false
-			timer.start()
-			Global.move_counter += 1
-	if event.is_action_released("move_up"):
-		can_move = true
-	if event.is_action_released("move_down"):
-		can_move = true
-	if event.is_action_released("move_left"):
-		can_move = true
-	if event.is_action_released("move_right"):
-		can_move = true
+			commit = true
+			
+	if commit:
+		can_move = false
+		timer.start()
+		Global.move_counter += 1
+
+# Function to handle player inputs
+func _input(event):
+	# Check to see if player has any moves left	
+	if Global.move_counter >= Global.total_moves:
+		pass
+	else:
+	# Loop through to see if the player has inputted any valid actions
+		for action in actions:
+			if Input.is_action_pressed(action) && can_move:
+				_commit_action(action)
+	
+	# Conditions to prevent conditious movement from a button being held down
+	for action in actions:
+		if event.is_action_released(action):
+			can_move = true
+			break
+	
 	
