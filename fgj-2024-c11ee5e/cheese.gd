43563613389f91406f38dd3c60fd27808ebb86c6
Fergus Currie <2309394@abertay.ac.uk>
2024-09-28 18:12:26 +0100

    shadow transparent

--- fgj-2024-c11ee5e/cheese.gd
+++ fgj-2024-c11ee5e/cheese.gd
@@ -1,16 +1,26 @@
-extends TileMap
+extends Node2D
 
 var holes = 2
+var prevlocx = 100
+var prevlocy =100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for x in 5:
 		for y in 5:
-			$".".set_cell(0, Vector2i(x,y),1,Vector2i(0,0))
+			$cheese.set_cell(0, Vector2i(x,y),1,Vector2i(0,0))
+			$shadow.set_cell(0, Vector2i(x,y),0,Vector2i(0,0))
 	for x in holes:
 		var xrand = randi_range(0,4)
+		while xrand == prevlocx:
+			xrand = randi_range(0,4)
 		var yrand = randi_range(0,4)
-		$".".set_cell(0, Vector2i(xrand,yrand),2,Vector2i(0,0))
-
+		while yrand == prevlocy:
+			yrand = randi_range(0,4)
+		$cheese.set_cell(0, Vector2i(xrand,yrand),2,Vector2i(0,0))
+		$shadow.set_cell(0, Vector2i(xrand,yrand),1,Vector2i(0,0))
+		prevlocx = xrand
+		prevlocy = yrand
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
