99d8bc03d53682afe41e680de4121f99f73da993
Noah-Morrison <145436571+Noah-Morrison@users.noreply.github.com>
2024-09-28 18:40:40 +0100

    Added Cheese Holes Variables

--- fgj-2024-c11ee5e/Global.gd
+++ fgj-2024-c11ee5e/Global.gd
@@ -1,10 +1,13 @@
 extends Node
 
 var move_counter = 0
-var total_moves = 5
+var total_moves = 8
 
 var move_counterP2 = 0
 var total_movesP2 = 5
+
+var cheese_holes = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
