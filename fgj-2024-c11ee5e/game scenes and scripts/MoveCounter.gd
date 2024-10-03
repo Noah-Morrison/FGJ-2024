09fb1d64543408b1755b4d7e6a475afea9a46328
Noah-Morrison <145436571+Noah-Morrison@users.noreply.github.com>
2024-10-03 17:57:47 +0100

    Added Names :3

--- fgj-2024-c11ee5e/game scenes and scripts/MoveCounter.gd
+++ fgj-2024-c11ee5e/game scenes and scripts/MoveCounter.gd
@@ -7,14 +7,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.text = str("Moves (P1): " , Global.move_counter , "/" , Global.total_moves, "\n",
-					"\n",
+					##"\n",
 					"Moves (P2): " , Global.move_counterP2 , "/" , Global.total_moves, "\n",
-					"\n",
-					"Position (P1): " , Global.player1_x + 1, "-", Global.player1_y, "\n",
-					"\n",
-					"Position (P2): " , Global.player2_x + 1, "-", Global.player2_y, "\n",
-					"\n",
-					"Cheese Hole 1: ", Global.cheese_holes[0][0] + 1, "-", Global.cheese_holes[0][1] + 1, "\n",
-					"Cheese Hole 2: ", Global.cheese_holes[1][0] + 1, "-", Global.cheese_holes[1][1] + 1, "\n",
-					"\n",
+					##"\n",
+					##"Position (P1): " , Global.player1_x + 1, "-", Global.player1_y, "\n",
+					##"\n",
+					##"Position (P2): " , Global.player2_x + 1, "-", Global.player2_y, "\n",
+					##"\n",
+					##"Cheese Hole 1: ", Global.cheese_holes[0][0] + 1, "-", Global.cheese_holes[0][1] + 1, "\n",
+					##"Cheese Hole 2: ", Global.cheese_holes[1][0] + 1, "-", Global.cheese_holes[1][1] + 1, "\n",
+					##"\n",
 					)
